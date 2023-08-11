181e80b058e5e334b918b78ae92d18839f5b1448
claudiaaziz <claudiahannahh@gmail.com>
2023-08-11 08:37:12 -0400

    tic tac toe projectgit add .

---
+++ tic_tac_toe_project/tic_tac_toe_v3/computer_player.rb
@@ -0,0 +1,14 @@
+class ComputerPlayer
+    attr_reader :mark
+
+    def initialize(val)
+        @mark = val
+    end
+
+    def get_position(legal_positions)
+        choice = legal_positions.sample
+        puts "Computer #{self.mark} chose position #{choice.to_s}"
+        choice
+    end
+
+end
